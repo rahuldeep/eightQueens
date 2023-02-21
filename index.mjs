9bcbed3d3f854eda9f695a017e5125410278fdf8
Rahul Deep <rahul.deep@gmail.com>
2023-02-21 12:28:42 -0600

    intitializing 2d array

--- index.mjs
+++ index.mjs
@@ -2,9 +2,35 @@
 
 export const handler = async(event,context) => {
     
+    var count = 1;
+    var chessBoard =[];
+
+    for (var i=0;i<2;i++)
+    {
+    var row = [];
+    for (var j=0;j<5;j++)
+    {
+        row.push("Test" + count);
+        count++;
+    }
+
+    chessBoard.push(row);
+
+    }
+    
+    
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Bambadaaaa!'),
+        body: JSON.stringify(chessBoard),
     };
     return response;
+
+
+
+
+
+
+
+
+
 };
