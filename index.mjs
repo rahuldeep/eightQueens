c796ebca65891fde7e29712f0f39a18065820dbd
Rahul Deep <rahul.deep@gmail.com>
2023-02-21 12:30:15 -0600

    intitializing 2d array

--- index.mjs
+++ index.mjs
@@ -5,12 +5,12 @@ export const handler = async(event,context) => {
     var count = 1;
     var chessBoard =[];
 
-    for (var i=0;i<2;i++)
+    for (var i=0;i<8;i++)
     {
     var row = [];
-    for (var j=0;j<5;j++)
+    for (var j=0;j<8;j++)
     {
-        row.push("Test" + count);
+        row.push(0);
         count++;
     }
 
