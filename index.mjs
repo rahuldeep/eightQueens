fec0e4ebb571609049f51950e76667137acb536e
Rahul Deep <rahul.deep@gmail.com>
2023-02-21 17:18:16 -0600

    main logic to put the queens shadow on the board and made board size variable

--- index.mjs
+++ index.mjs
@@ -62,26 +62,27 @@ export const handler = async(event,context) => {
 function markQueensShadow(column,row,counter){
     for (i=0;i<boardSize;i++){
         chessBoard[i][row]+=counter;
-        console.log ("shadow @" + column + row);
+        console.log ("shadow @" + i + row);
      }
      chessBoard[column][row]-=2*counter;
      for (i=0;i<boardSize;i++){
         chessBoard[column][i] +=counter;
-        console.log ("shadow @" + column + row);
+        console.log ("shadow @" + column + i);
         let leftOffset = i-row;
         let rightOffset = row-i;
         let leftOffsetColumn = column+leftOffset;
         let rightOffsetColumn = column+rightOffset;
-        if (leftOffsetColumn<0 && leftOffsetColumn>boardSize-1){
+        if (leftOffsetColumn>0 && leftOffsetColumn<boardSize-1){
             chessBoard[leftOffsetColumn][i] += counter; 
             console.log ("shadow @" + leftOffsetColumn + i); 
         }
-        if (rightOffsetColumn<0 && rightOffsetColumn>boardSize-1){
+        if (rightOffsetColumn>0 && rightOffsetColumn<boardSize-1){
             chessBoard[rightOffsetColumn][i] += counter;  
             console.log ("shadow @" + rightOffsetColumn + i); 
         }
      }
      chessBoard[column][row]-=2*counter;
+     console.log(chessBoard);
      
      }
 
