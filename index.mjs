f03152658a6afe2d8b75e04b0928c20ae24c00cb
Rahul Deep <rahul.deep@gmail.com>
2023-02-21 17:41:09 -0600

    main logic to put the queens shadow on the board and made board size variable

--- index.mjs
+++ index.mjs
@@ -39,7 +39,7 @@ export const handler = async(event,context) => {
                     row--;
                     chessBoard[lastPlacedColumn][row] = 0;
                     markQueensShadow (lastPlacedColumn,row,-1)
-                    column = lastPlacedColumn+1;
+                    if (column < 7 ){column = lastPlacedColumn+1} else{column=0};
 
                 } else {
                     column++;
@@ -63,23 +63,23 @@ export const handler = async(event,context) => {
 function markQueensShadow(column,row,counter){
     for (i=0;i<boardSize;i++){
         chessBoard[i][row]+=counter;
-        console.log ("shadow @" + i + row);
+        //console.log ("shadow @" + i + row);
      }
      chessBoard[column][row]-=2*counter;
      for (i=0;i<boardSize;i++){
         chessBoard[column][i] +=counter;
-        console.log ("shadow @" + column + i);
+        //console.log ("shadow @" + column + i);
         let leftOffset = i-row;
         let rightOffset = row-i;
         let leftOffsetColumn = column+leftOffset;
         let rightOffsetColumn = column+rightOffset;
         if (leftOffsetColumn>0 && leftOffsetColumn<boardSize-1){
             chessBoard[leftOffsetColumn][i] += counter; 
-            console.log ("shadow @" + leftOffsetColumn + i); 
+            //console.log ("shadow @" + leftOffsetColumn + i); 
         }
         if (rightOffsetColumn>0 && rightOffsetColumn<boardSize-1){
             chessBoard[rightOffsetColumn][i] += counter;  
-            console.log ("shadow @" + rightOffsetColumn + i); 
+            //console.log ("shadow @" + rightOffsetColumn + i); 
         }
      }
      chessBoard[column][row]-=2*counter;
