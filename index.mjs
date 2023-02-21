47095f2a3e1223e80159eba3e778ad8f7cd680b7
Rahul Deep <rahul.deep@gmail.com>
2023-02-21 11:54:22 -0600

    first commit

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambadaa!'),
+        body: JSON.stringify('Hello from Lambadaaaa!'),
     };
     return response;
 };
