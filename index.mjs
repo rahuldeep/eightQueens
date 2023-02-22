77b57848324d11dd8d5a70cfbb568a5f01fd3553
Rahul Deep <rahul.deep@gmail.com>
2023-02-22 17:42:09 -0600

    resolving issue of recursive loop

--- index.mjs
+++ index.mjs
@@ -6,16 +6,18 @@ export const handler = async(event,context) => {
     //INTITIALIZE board ARRAY
     var count = 1;
     var chessBoard =[];
+    var solution =[];
 
     for (var i=0;i<boardSize;i++)
     {
-    var chessRow = [];
-    for (var j=0;j<boardSize;j++)
-    {
-        //fill with 0s
-        chessRow.push(0);
-    }
-    chessBoard.push(chessRow);
+        var chessRow = [];
+        for (var j=0;j<boardSize;j++)
+            {
+                //fill with 0s
+                chessRow.push(0);
+            }
+        chessBoard.push(chessRow);
+        solution.push(0);
     }
     //set up variables to check progress and hold intermediary values
     let solved = false;
@@ -29,33 +31,16 @@ export const handler = async(event,context) => {
         while (!placed){
             //check to see if current position is open
             if (chessBoard[column][row] == 0){
-                chessBoard[column][row] = "X";
-                placed = true;
-                console.log('placed row ' + row+' column ' + column);
-                //mark all areas that are blocked because of 
-                //the current placement
-                markQueensShadow(column,row,1);
-                lastPlacedColumn = column;
-                row++;
-                column = 0;
+                addPiece(column,row);
             } else {
                 //check to see if we have run out columns for the current row without
                 //finding an open spot
                 if (column ==boardSize-1){
-                    //go back a row
-                    if (row=0){solved=true};
-                    row--;
-                    //remove the last placed queen
-                    chessBoard[lastPlacedColumn][row] = 0;
-                    console.log('removed row ' + row+' column ' + lastPlacedColumn);
-                    //put the next column from the last placed as the active
-                    column=lastPlacedColumn+1;
-                    //unmark all areas that are now open
-                    //
-                    markQueensShadow (lastPlacedColumn,row,-1);
+                    removePiece(column,row,lastPlacedColumn);
                     //set current column to last one + 1
-                    //or wrap it around to 0
-                    if (column < boardSize-1 ){column = lastPlacedColumn+1} else{column=0};
+                    //or call removePiece again for the last row
+                    if (column < boardSize-1 ){column = lastPlacedColumn+1
+                    } else{removePiece(column,row,solution[row-1])};
 
                 } else {
                     column++;
@@ -78,6 +63,37 @@ export const handler = async(event,context) => {
     return response;
 
 
+//adds a new queent o the board and marks all shadow cells    
+function addPiece(column,row){
+    chessBoard[column][row] = "X";
+    placed = true;
+    //add column number to solution array
+    solution[row] = 1;
+    console.log('placed row ' + row+' column ' + column);
+    console.log ('solution till now' + solution);
+    //mark all areas that are blocked because of 
+    //the current placement
+    markQueensShadow(column,row,1);
+    lastPlacedColumn = column;
+    row++;
+    column = 0;
+}
+
+function removePiece(column,row,lastPlacedColumn){
+    //go back a row
+    row--;
+    //remove the last placed queen
+    chessBoard[lastPlacedColumn][row] = 0;
+    //remove it from solution
+    solution[row] = 0;
+    console.log('removed row ' + row+' column ' + lastPlacedColumn);
+    //put the next column from the last placed as the active
+    //column=lastPlacedColumn+1;
+    //unmark all areas that are now open
+    //
+    markQueensShadow (lastPlacedColumn,row,-1);
+    
+}
 
 function markQueensShadow(column,row,counter){
     
